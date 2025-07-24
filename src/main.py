d65c1343c1a66e18247edd1ec046dd45f6b1e489
Cristopher <andresaedo032@gmail.com>
2025-07-23 23:54:04 -0400

    mod: Importar nuevas funciones y condicionar insercion de datos

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from BDD import connect_db, crear_tabla_profesor, crear_tablas_consulta
+from BDD import connect_db, crear_tabla_profesor, crear_tablas_consulta, insercion_datos, datos_existentes
 from iu import main_iu
 
 def main():
@@ -9,6 +9,12 @@ def main():
     
     crear_tabla_profesor(connect)
     crear_tablas_consulta(connect)
+    
+
+    if not datos_existentes(connect):
+        insercion_datos(connect)
+    else:
+        print("\nLos datos ya existen en la base de datos.\n")
 
     try:
         main_iu(connect)
